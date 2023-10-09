68500b48ffebe9f01d54ef013dd4d73632693530
Danilo Formagio <daniloformagio@gmail.com>
2023-10-08 23:20:26 -0300

    Configure prettier for tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl">Hello Tailwind</h1>
+    <div className="h-screen bg-slate-900 p-8 text-slate-100">
+      <h1 className="text-5xl font-bold">Hello Tailwind</h1>
       <h2>Hello Tailwind</h2>
       <p>Hello Tailwind</p>
     </div>
