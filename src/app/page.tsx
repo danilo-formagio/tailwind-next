02a580969ea35be7861240e551b34e6529a97781
Danilo Formagio <daniloformagio@gmail.com>
2023-10-09 23:01:03 -0300

    Creating layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-slate-900 p-8 text-slate-100">
-      <h1 className="text-5xl font-bold">Hello Tailwind</h1>
-      <h2>Hello Tailwind</h2>
-      <p>Hello Tailwind</p>
-    </div>
-  );
+  return <h1>Hello Tailwind</h1>;
 }
