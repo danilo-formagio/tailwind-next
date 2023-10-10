5085641dab7acfa86239d656e1c773c12cdd227c
Danilo Formagio <daniloformagio@gmail.com>
2023-10-10 12:33:52 -0300

    Create logo and search input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from '@/components/Sidebar';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -13,8 +14,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="grid-cols-app grid min-h-screen">
-          <aside className="border-r border-zinc-200 px-5 py-8">sidebar</aside>
+        <div className="grid min-h-screen grid-cols-app">
+          <Sidebar />
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
       </body>
