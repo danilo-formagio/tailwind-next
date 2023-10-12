1b31bc9f4df89bddf62e977a9d829024f398226b
Danilo Formagio <daniloformagio@gmail.com>
2023-10-11 23:24:13 -0300

    Creating tabs using Radix tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
