f553bc150c82db37ab0d3455808fc798387288a7
Danilo Formagio <daniloformagio@gmail.com>
2023-10-20 22:21:39 -0300

    Adding ghost variant for some buttons

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,8 +1,9 @@
 import { LogOut } from 'lucide-react';
+import { Button } from '../Button';
 
 export function Profile() {
   return (
-    <div className="grid-cols-profile grid items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-3">
       <img src="https://github.com/danilo-formagio.png" className="h-10 w-10 rounded-full" alt="" />
 
       <div className="flex flex-col truncate">
@@ -10,9 +11,9 @@ export function Profile() {
         <span className="truncate text-sm text-zinc-500">danilo_formagio@outlook.com</span>
       </div>
 
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   );
 }
